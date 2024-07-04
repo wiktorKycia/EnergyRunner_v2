cb2dee4de5fa27362ab3f42642e144b20dbbdf11
Wiktor Kycia <wiktor.kycia@gmail.com>
2024-07-04 20:58:14 +0200

    add Board 2-dimensional array, add private randomizer, in constructor, initialize the Board and fill it with random values from -9 to 9

--- classes/Game.cs
+++ classes/Game.cs
@@ -9,9 +9,19 @@ namespace EnergyRunner_v2.classes
     internal class Game
     {
         public int Size { get; set; }
+        public int[,] Board { get; set; }
+        private Random randomizer = new Random();
         public Game(int size)
         {
             Size = size;
+            Board = new int[Size, Size];
+            for(int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    Board[i, j] = randomizer.Next(-9, 9);
+                }
+            }
         }
 
     }

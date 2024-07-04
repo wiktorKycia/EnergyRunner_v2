94fce1b4c2ed826b142d6f7220da88a5574bbf99
Wiktor Kycia <wiktor.kycia@gmail.com>
2024-07-04 20:39:12 +0200

    first commit, add gitignore with Program.cs in which I added previously some comments what to do next

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace EnergyRunner_v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+            //na samym początku wyświetla się plansza z grą
+            //jest pod nią instrukcja
+            //jak gracz wykona ruch to plansza się resetuje
+            //i tak w kółko aż nie dojdzie do celu
+            //jak gracz wygra to gra go nagradza komplementem i się go pyta czy chce grać dalej
+            //jak tak to tworzy nową grę, 
+            //a jak nie to wychodzi z gry
+        }
+    }
+}

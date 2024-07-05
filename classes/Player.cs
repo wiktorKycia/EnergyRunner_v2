07f341c4ec49abce8097e7405e80544ee16af69f
Wiktor Kycia <wiktor.kycia@gmail.com>
2024-07-05 13:28:18 +0200

    add Player class with overwritten properties of BoardObject, add Direction enum with 4 basic directions, add method Move to Player class, that changes its position based on the given direction

---
+++ classes/Player.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnergyRunner_v2.classes
+{
+    enum Direction
+    {
+        Up, Right, Down, Left
+    }
+
+    internal class Player : BoardObject
+    {
+        public override int PositionX { get; set; }
+        public override int PositionY { get; set; }
+
+        public void Move(Direction direction)
+        {
+            switch ((int)direction)
+            {
+                case 1:
+                    PositionY--;
+                    break;
+                case 2:
+                    PositionX++;
+                    break;
+                case 3:
+                    PositionY++;
+                    break;
+                case 4:
+                    PositionX--;
+                    break;
+                default:
+                    throw new Exception("Something went wrong, probably you tried to move in an unknown directoin");
+            }
+        }
+    }
+}

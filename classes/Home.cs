b0f25575d844a27752c5307f181389c3f55396be
Wiktor Kycia <wiktor.kycia@gmail.com>
2024-07-05 13:40:41 +0200

    add class Home, that inherits the same properties as Player, add constructor which initializes x and y positions

---
+++ classes/Home.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnergyRunner_v2.classes
+{
+    internal class Home : BoardObject
+    {
+        public override int PositionX { get; set; }
+        public override int PositionY { get; set; }
+
+        public Home(int x, int y)
+        {
+            PositionX = x;
+            PositionY = y;
+        }
+    }
+}

af36fcb08ce244f7d30ca376059fb764e7a172d8
Wiktor Kycia <wiktor.kycia@gmail.com>
2024-07-07 13:35:35 +0200

    add mechanics to rerun the game after the game is finished, add 'you won' and 'you lost' comments after the game

--- Program.cs
+++ Program.cs
@@ -6,50 +6,82 @@ namespace EnergyRunner_v2
         static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.Unicode;
-            int size = GetUserInput("How big would you like to have a map? ");
-            Game game = new Game(size);
-            do
+            bool running = true;
+            while (running)
             {
-                game.UpdateBoard();
-                game.DisplayBoard();
-                game.GiveInstructions();
-
-                //if (Console.KeyAvailable)
-                //{
-                var key = Console.ReadKey(true);
-                if (key.Key == ConsoleKey.UpArrow && game.Player.PositionY != 0)
-                {
-                    game.Player.Move(Direction.Up);
-                    game.Player.Energy += game.Board[game.Player.PositionY, game.Player.PositionX];
-                }
-                else if (key.Key == ConsoleKey.DownArrow && game.Player.PositionY != game.Size - 1)
-                {
-                    game.Player.Move(Direction.Down);
-                    game.Player.Energy += game.Board[game.Player.PositionY, game.Player.PositionX];
-                }
-                else if (key.Key == ConsoleKey.RightArrow && game.Player.PositionX != game.Size - 1)
-                {
-                    game.Player.Move(Direction.Right);
-                    game.Player.Energy += game.Board[game.Player.PositionY, game.Player.PositionX];
-                }
-                else if (key.Key == ConsoleKey.LeftArrow && game.Player.PositionX != 0)
-                {
-                    game.Player.Move(Direction.Left);
-                    game.Player.Energy += game.Board[game.Player.PositionY, game.Player.PositionX];
-                }
-                //}
                 Console.Clear();
+                int size = GetUserInput("How big would you like to have a map? ");
+                Game game = new Game(size);
+                do
+                {
+                    if (game.Player.PositionX == game.Home.PositionX && game.Player.PositionY == game.Home.PositionY)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("You won!");
+                        Console.ResetColor();
+                        Console.Write("Do you want to play again? (write 'Yes' to play again): ");
+                        string answer = Console.ReadLine();
+                        if (answer != "Yes")
+                        {
+                            running = false; 
+                        }
+                        GC.Collect();
+                        break;
+                    }
+                    else if (game.Player.Energy <= 0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("You lost!");
+                        Console.ResetColor();
+                        Console.Write("Do you want to play again? (write 'Yes' to play again): ");
+                        string answer = Console.ReadLine();
+                        if (answer != "Yes")
+                        {
+                            running = false;
+                        }
+                        GC.Collect();
+                        break;
+
+                    }
+                    game.UpdateBoard();
+                    game.DisplayBoard();
+                    game.GiveInstructions();
+
+                    var key = Console.ReadKey(true);
+                    if (key.Key == ConsoleKey.UpArrow && game.Player.PositionY != 0)
+                    {
+                        game.Player.Move(Direction.Up);
+                        game.Player.Energy += game.Board[game.Player.PositionY, game.Player.PositionX];
+                    }
+                    else if (key.Key == ConsoleKey.DownArrow && game.Player.PositionY != game.Size - 1)
+                    {
+                        game.Player.Move(Direction.Down);
+                        game.Player.Energy += game.Board[game.Player.PositionY, game.Player.PositionX];
+                    }
+                    else if (key.Key == ConsoleKey.RightArrow && game.Player.PositionX != game.Size - 1)
+                    {
+                        game.Player.Move(Direction.Right);
+                        game.Player.Energy += game.Board[game.Player.PositionY, game.Player.PositionX];
+                    }
+                    else if (key.Key == ConsoleKey.LeftArrow && game.Player.PositionX != 0)
+                    {
+                        game.Player.Move(Direction.Left);
+                        game.Player.Energy += game.Board[game.Player.PositionY, game.Player.PositionX];
+                    }
+                    Console.Clear();
+                }
+                while (true);
+                //pytanie do gracza jak duża mapa
+                //na samym początku wyświetla się plansza z grą
+                //jest pod nią instrukcja
+                //jak gracz wykona ruch to plansza się resetuje
+                //i tak w kółko aż nie dojdzie do celu
+                //jak gracz wygra to gra go nagradza komplementem i się go pyta czy chce grać dalej
+                //jak tak to tworzy nową grę, 
+                //a jak nie to wychodzi z gry
             }
-            while (true);
-            //pytanie do gracza jak duża mapa
-            //na samym początku wyświetla się plansza z grą
-            //jest pod nią instrukcja
-            //jak gracz wykona ruch to plansza się resetuje
-            //i tak w kółko aż nie dojdzie do celu
-            //jak gracz wygra to gra go nagradza komplementem i się go pyta czy chce grać dalej
-            //jak tak to tworzy nową grę, 
-            //a jak nie to wychodzi z gry
         }
+            
         static int GetUserInput(string message)
         {
             int result = 0;
